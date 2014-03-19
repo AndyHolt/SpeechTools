1f57e17dd18fd22d8345d78cd0bd34241a386989
Andy Holt <andrew.holt@hotmail.co.uk>
2014-03-19 16:29:47 +0000

    Forward/backward, state likelihood and viterbi all completed.

--- BaumWelchEstimation.py
+++ BaumWelchEstimation.py
@@ -99,6 +99,101 @@ class BaumWelch(object):
       partialSum += (self.alpha[k,-1] * self.transitionMatrix[k,-1])
     self.alpha[-1,-1] = partialSum
 
+    # likelihood of observed sequence, p(O|lambda)
+    self.observationLikelihood = self.alpha[-1,-1]
+
+  def backwardVariableGeneration(self):
+    """
+    Calculate backward variables, beta
+    """
+    self.beta = zeros((self.noOfEmmittingStates+2, self.T + 1))
+
+    # initialisation
+    for j in range(self.noOfEmmittingStates+1):
+      self.beta[j,-1] = self.transitionMatrix[j,-1]
+    self.beta[-1,-1] = 1.0
+
+    # main recursion
+    for t in range(self.T, 1, -1):
+      for j in range(self.noOfEmmittingStates, 0, -1):
+        partialSum = 0
+        for k in range(1, self.noOfEmmittingStates+1):
+          partialSum += (self.transitionMatrix[j,k-1] * self.b[k-1,t-1] * self.beta[k,t])
+        self.beta[j,t-1] = partialSum
+
+    # first column
+    partialSum = 0
+    for k in range(1, self.noOfEmmittingStates+1):
+      partialSum += (self.transitionMatrix[0,k-1] * self.b[k-1,0] * self.beta[k,1])
+    self.beta[0,0] = partialSum
+
+    # likelihood of observed sequence, p(O|lambda)
+    self.observationLikelihood = self.alpha[-1,-1]
+
+  def  checkForwardBackwardUnity(self):
+    """
+    Check that forward and backward algorithms agree on p(O|lambda)
+    """
+    if round(self.alpha[-1,-1] - self.beta[0, 0], 7) != 0:
+      print('')
+      raise BaumWelchForwardBackwardDifference(self.alpha[-1,-1], self.beta[0,0])
+
+  def stateOccupationProbabilityGeneration(self):
+    """
+    Calculate the a-posteriori state occupation probabililty.
+
+    Probability that the HHM was in the given state at the given time step.
+    """
+    self.L = zeros((self.noOfEmmittingStates, self.T))
+
+    for j in range(self.noOfEmmittingStates):
+      for t in range(self.T):
+        self.L[j,t] = (self.alpha[j+1, t+1] * self.beta[j+1, t+1]) / self.observationLikelihood
+
+  def viterbi(self):
+    """
+    Find most likely state sequence thorugh the HMM
+    """
+    # initialisation
+    self.phi = zeros((self.noOfEmmittingStates+2, self.T + 1))
+    self.phi[0,0] = 1.0
+    for i in range(1,self.noOfEmmittingStates+2):
+      self.phi[i,0] = 0.0
+    for t in range(1,self.T+1):
+      self.phi[0,t] = 0.0
+    self.traceback = zeros((self.noOfEmmittingStates+1, self.T+1))
+
+    # main recursion
+    for t in range(1, self.T + 1):
+      for j in range(1, self.noOfEmmittingStates + 1):
+        phiTemp = zeros((self.noOfEmmittingStates + 1, 1))
+        for k in range(self.noOfEmmittingStates+1):
+          phiTemp[k,0] = self.phi[k,t-1] * self.transitionMatrix[k, j-1]
+        self.traceback[j-1,t-1] = nonzero(phiTemp == phiTemp.max(0))[0][0]
+        self.phi[j, t] = phiTemp.max(0) * self.b[j-1, t-1]
+
+    # last column - set states which can't reach term to 0, sub for term
+    for j in range(1,self.noOfEmmittingStates + 1):
+      if self.transitionMatrix[j,-1] == 0:
+        self.phi[j,-1] = 0
+    phiTemp = zeros((self.noOfEmmittingStates+1, 1))
+    for k in range(self.noOfEmmittingStates + 1):
+      phiTemp[k,0] = self.phi[k,-1] * self.transitionMatrix[k,-1]
+    self.traceback[-1,-1] = nonzero(phiTemp == phiTemp.max(0))[0][0]
+    self.phi[-1,-1] = phiTemp.max(0)
+
+  def getTraceback(self):
+    """
+    Get most likely path
+    """
+    self.mostLikelyPath = zeros((1, self.T+2))
+
+    self.mostLikelyPath[0,0] = 0
+    self.mostLikelyPath[0,-1] = self.noOfEmmittingStates+1
+
+    for s in range(self.T, 0, -1):
+      self.mostLikelyPath[0,s] = self.traceback[self.mostLikelyPath[0,s+1]-1, s]
+
   def gaussianDist(self, x, mu, var):
     """
     Return the probability at point x for a gaussian/normal distribuion of given parameters
@@ -115,3 +210,16 @@ class BaumWelchInputError(Exception):
     self.value = value
   def __str__(self):
     return repr(self.value)
+
+class BaumWelchForwardBackwardDifference(Exception):
+  """
+  Exceptions for calculation errors in BW class
+  """
+  def __init__(self, fwdVal, bkdVal):
+    self.fwdVal = fwdVal
+    self.bkdVal = bkdVal
+  def __str__(self):
+    s = 'Forward and backward algorithms don\'t agree on observation ' + \
+        'likelihood \n' + 'Forward value: \t' + repr(self.fwdVal) + \
+        '\n Backward value: \t' + repr(self.bkdVal)
+    return s
